d522cd28328f4a76471b0156161ea746f10d79e3
Mafe0394 <mfvargasm0394@gmail.com>
2021-10-10 20:47:59 -0400

    testing navigation with mockito and espresso

---
+++ app/src/androidTest/java/com/projects/shoppinglisttestingtutorial/ui/ShoppingFragmentTest.kt
@@ -0,0 +1,49 @@
+package com.projects.shoppinglisttestingtutorial.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.MediumTest
+import com.projects.shoppinglisttestingtutorial.R
+import com.projects.shoppinglisttestingtutorial.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+
+@MediumTest
+@HiltAndroidTest
+@ExperimentalCoroutinesApi
+class ShoppingFragmentTest{
+
+    @get:Rule
+    val hiltRule=HiltAndroidRule(this)
+
+    @Before
+    fun setup(){
+        hiltRule.inject()
+    }
+
+    @Test
+    fun clickAddShoppingItem_navigateToShoppingItemFragment(){
+        val navController=mock(NavController::class.java)
+
+        launchFragmentInHiltContainer<ShoppingFragment> {
+            Navigation.setViewNavController(requireView(),navController)
+        }
+
+        onView(withId(R.id.fabAddShoppingItem)).perform(click())
+
+        verify(navController).navigate(
+            ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+        )
+    }
+}
--- app/src/main/java/com/projects/shoppinglisttestingtutorial/ui/ShoppingFragment.kt
+++ app/src/main/java/com/projects/shoppinglisttestingtutorial/ui/ShoppingFragment.kt
@@ -1,10 +1,22 @@
 package com.projects.shoppinglisttestingtutorial.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.projects.shoppinglisttestingtutorial.R
+import kotlinx.android.synthetic.main.fragment_shopping.*
 
 class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
 
-    private val viewModel:ShoppingViewModel by activityViewModels()
+    private val viewModel: ShoppingViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabAddShoppingItem.setOnClickListener {
+            findNavController().navigate(ShoppingFragmentDirections
+                .actionShoppingFragmentToAddShoppingItemFragment())
+        }
+    }
 }
